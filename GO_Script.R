713ba4cae45c92d72e7b25f206f3896ae91df536
Tristan Jafari <tristanjafari@Tristans-MacBook-Air-3.local>
2025-12-12 14:26:23 +0000

    fef

--- GO_Script.R
+++ GO_Script.R
@@ -3,19 +3,48 @@
 
 install.packages("openxlsx")
 library(openxlsx)
-A.data<-read.xlsx("A.xlsx")
-B.data<-read.xlsx("B.xlsx")
-P.data<-read.xlsx("P.xlsx")
-H.data<-read.csv("H.csv")
-SE.data<-read.xlsx("SouthEast.xlsx")
+install.packages("readxl")
+library(readxl)
+install.packages("dplyr")
+library(dplyr)
+A.data <- read_xlsx("data/A.xlsx")
+B.data <- read_xlsx("data/B.xlsx")
+P.data <- read_xlsx("data/P.xlsx")
+H.data <- read_xlsx("data/H.csv")
+SE.data <- read_xlsx("data/SouthEast.xlsx")
 
+#Cleaning A
+dim(A.data)
+str(A.data)
+head(A.data)
+
+A.data$PH <- ifelse(A.data$PH == "Y", 1, ifelse(A.data$PH == "N", 0, A.data$PH))
+A.data$MH <- ifelse(A.data$MH == "Y", 1, ifelse(A.data$MH == "N", 0, A.data$MH))
+A.data$Smoker <- ifelse(A.data$Smoker == "Y", 1, ifelse(A.data$Smoker == "Current", 1, ifelse(A.data$Smoker == "N", 0, A.data$MH)))
 A.data$SES5 <- ifelse(
-  A.data$SES5 == "SES1", 1,
-  ifelse(A.data$SES5 == "SES2", 2,
-         ifelse(A.data$SES5 == "SES3", 3,
-                ifelse(A.data$SES5 == "SES4", 4,
-                       ifelse(A.data$SES5 == "SES5", 5, A.data$SES5)
+  A.data$SES5 == "L1", 1,
+  ifelse(A.data$SES5 == "L2", 2,
+         ifelse(A.data$SES5 == "L3", 3,
+                ifelse(A.data$SES5 == "L4", 4,
+                       ifelse(A.data$SES5 == "L5", 5, A.data$SES5)
                 )
          )
   )
 )
+
+dim(A.data)
+str(A.data)
+head(A.data)
+
+#Cleaning B
+
+#Cleaning P
+
+#Cleaning H
+
+
+#Merging Data
+Combined.Data <- bind_rows(A.data, B.data, H.data, P.data)
+
+
+#Binomial Regression
