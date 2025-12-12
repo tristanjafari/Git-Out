d92261a7f1604f9417fc8c1f4454e3a66c3b7f20
Julia Eden <juliaeden@Julias-MacBook-Air-188.local>
2025-12-12 13:48:58 +0000

    Loading data

--- GO_Script.R
+++ GO_Script.R
@@ -1,2 +1,10 @@
 #This is Tristan confirming our git is working. I love my group
 #Hi tristan I also love my group x
+
+install.packages("openxlsx")
+library(openxlsx)
+A.data<-read.xlsx("A.xlsx")
+B.data<-read.xlsx("B.xlsx")
+P.data<-read.xlsx("P.xlsx")
+H.data<-read.csv("H.csv")
+SE.data<-read.xlsx("SouthEast.xlsx")
