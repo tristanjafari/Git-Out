04c791385586fd77e01680f09f9351c13142930a
Tristan Jafari <tristanjafari@Tristans-MacBook-Air-3.local>
2025-12-12 14:12:29 +0000

    f

--- GO_Script.R
+++ GO_Script.R
@@ -8,3 +8,14 @@ B.data<-read.xlsx("B.xlsx")
 P.data<-read.xlsx("P.xlsx")
 H.data<-read.csv("H.csv")
 SE.data<-read.xlsx("SouthEast.xlsx")
+
+A.data$SES5 <- ifelse(
+  A.data$SES5 == "SES1", 1,
+  ifelse(A.data$SES5 == "SES2", 2,
+         ifelse(A.data$SES5 == "SES3", 3,
+                ifelse(A.data$SES5 == "SES4", 4,
+                       ifelse(A.data$SES5 == "SES5", 5, A.data$SES5)
+                )
+         )
+  )
+)
